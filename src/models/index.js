68b6da4dbd43d693bddfeae67318ff198870b0c0
Fakhri Prayatna <50196922+fakhripraya@users.noreply.github.com>
2023-04-04 16:06:27 +0700

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ const { db } = require("../config");
 require("./user/master_user");
 
 const InitModels = async () => {
-    await db.sync({ alter: true, force: true })
+    await db.sync({ alter: true, force: false })
         .then(() => {
             console.log("All models has been synchronized successfully.");
         }).catch((err) => {

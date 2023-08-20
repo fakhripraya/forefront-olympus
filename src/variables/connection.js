576f36ed980b9587c3f0d445e77a47634e2c87a8
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-08-20 22:07:35 +0700

    Update connection.js

--- src/variables/connection.js
+++ src/variables/connection.js
@@ -1,5 +1,7 @@
 const APP_ORIGIN = process.env.APP_ORIGIN.split(' ') || ["http://localhost:3000"];
 
+console.log("APP ORIGINS: ");
+console.log(APP_ORIGIN);
 module.exports = {
     ALLOW_LIST: APP_ORIGIN
 }
